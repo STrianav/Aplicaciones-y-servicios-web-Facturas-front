a036e88140194f403aca9ab4219b03ddfaa8fb49
Santiago Ruiz <santiagoruiz308363@correo.itm.edu.co>
2025-02-25 21:32:21 -0500

    Trabajo Santiago-Ruiz

--- src/app/web/navigation/contenido/represenvisual/represenvisual.component.ts
+++ src/app/web/navigation/contenido/represenvisual/represenvisual.component.ts
@@ -1,11 +1,60 @@
 import { Component } from '@angular/core';
+import { firstValueFrom } from 'rxjs';
+import { ApiService } from '../../../../Api/api.service';
+import { FormularioService } from '../../Formulario/formulario.service';
+import { ModuloGeneralModule } from '../../../../shared/modulo-general.module';
 
 @Component({
   selector: 'app-represenvisual',
-  imports: [],
+  imports: [ModuloGeneralModule],
   templateUrl: './represenvisual.component.html',
   styleUrl: './represenvisual.component.css'
 })
 export class RepresenvisualComponent {
+  data: any;
+  Tabla: string = 'frecuencia';
 
+  constructor(private api: ApiService,
+     private modal: FormularioService) { }
+
+  ngOnInit(): void {
+    this.TraerTabla();
+  }
+
+  async TraerTabla() {
+    try{
+      let data = await firstValueFrom(this.api.TraerTabla(this.Tabla));
+      this.data = data;
+    } catch (error) {
+      console.error('Error al obtener la tabla:', error);
+    }
+  }
+
+  Crudd(tipo: 'C' | 'U' | 'D', row?: any) {
+    let formSchema: any;
+    if (tipo === 'C') {
+      formSchema = {
+        fields: [
+          { name: 'nombre', label: 'Nombre', type: 'text', value: '', validators: ['required'] }
+        ]
+      };
+    } else if (tipo === 'U') {
+      formSchema = {
+        fields: [
+          { name: 'nombre', label: 'Nombre', type: 'text', value: '', validators: ['required'] }
+        ]
+      };
+    } else if (tipo === 'D') {
+      formSchema = {
+        fields: [
+          { name: 'nombre', label: 'Nombre', type: 'text', value: '', validators: ['required'] }
+        ]
+      };
+    }
+    const modalRef = this.modal.openDynamicModal(formSchema, tipo, this.Tabla);
+
+    modalRef.result.then(() => {
+      this.TraerTabla();
+    });
+  }
 }
